52713832cf4ed9aa5717b4735c85de0cebd91a3e
trbrooks <tim.r.brooks@nasa.gov>
2022-09-12 12:29:25 -0400

    first attempt at openvsp package

---
+++ openvsp/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $(uname) == Darwin ]]; then
+  export OS="MacOS"
+elif [[ "$target_platform" == linux-* ]]; then
+  export OS="Linux"
+fi
+
+mkdir build buildlibs
+cd buildlibs
+cmake -DVSP_USE_SYSTEM_LIBXML2=true -DVSP_USE_SYSTEM_FLTK=true -DVSP_USE_SYSTEM_GLM=true -DVSP_USE_SYSTEM_GLEW=true -DVSP_USE_SYSTEM_CMINPACK=true -DVSP_USE_SYSTEM_LIBIGES=false -DVSP_USE_SYSTEM_EIGEN=true -DVSP_USE_SYSTEM_CODEELI=false -DVSP_USE_SYSTEM_CPPTEST=false ../Libraries -DCMAKE_BUILD_TYPE=Release
+make -j ${CPU_COUNT}
+#make DESTDIR=${PREFIX}/libs install
+cd ../build
+cmake ../src/ -DVSP_LIBRARY_PATH=../buildlibs -DCMAKE_BUILD_TYPE=Release
+make -j ${CPU_COUNT}
+make package
+# Install python interface
+pushd _CPack_Packages/${OS}/ZIP/OpenVSP-${version}-${OS}/python
+pip install -r requirements.txt
+pushd ..
+cp openvsp vspaero vspscript vspslicer ${PREFIX}/bin
+popd
+popd
