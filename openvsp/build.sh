07abb385c4bc6816e6e2770d8a394d5ad5561837
trbrooks <tim.r.brooks@nasa.gov>
2022-09-13 11:25:32 -0400

    Adding config file to build w/o graphics

--- openvsp/build.sh
+++ openvsp/build.sh
@@ -8,18 +8,22 @@ fi
 
 mkdir build buildlibs
 cd buildlibs
-cmake -DVSP_USE_SYSTEM_LIBXML2=true -DVSP_USE_SYSTEM_FLTK=true -DVSP_USE_SYSTEM_GLM=true -DVSP_USE_SYSTEM_GLEW=true -DVSP_USE_SYSTEM_CMINPACK=true -DVSP_USE_SYSTEM_LIBIGES=false -DVSP_USE_SYSTEM_EIGEN=true -DVSP_USE_SYSTEM_CODEELI=false -DVSP_USE_SYSTEM_CPPTEST=false ../Libraries -DCMAKE_BUILD_TYPE=Release
-make -j ${CPU_COUNT}
-#make DESTDIR=${PREFIX}/libs install
+cmake -DVSP_NO_GRAPHICS=${no_graphics} -DVSP_USE_SYSTEM_LIBXML2=true -DVSP_USE_SYSTEM_FLTK=true \
+      -DVSP_USE_SYSTEM_GLM=true -DVSP_USE_SYSTEM_GLEW=true \
+      -DVSP_USE_SYSTEM_CMINPACK=true -DVSP_USE_SYSTEM_LIBIGES=false -DVSP_USE_SYSTEM_EIGEN=true \
+      -DVSP_USE_SYSTEM_CODEELI=false -DVSP_USE_SYSTEM_CPPTEST=false ../Libraries -DCMAKE_BUILD_TYPE=Release
+make -j ${CPU_COUNT} VERBOSE=1
 cd ../build
-cmake ../src/ -DVSP_LIBRARY_PATH=../buildlibs -DCMAKE_BUILD_TYPE=Release
-make -j ${CPU_COUNT}
+cmake ../src/ -DVSP_NO_GRAPHICS=${no_graphics} -DVSP_LIBRARY_PATH=../buildlibs -DCMAKE_BUILD_TYPE=Release
+make -j ${CPU_COUNT} VERBOSE=1
 make package
 # Install python interface
 pushd _CPack_Packages/${OS}/ZIP/OpenVSP-${PKG_VERSION}-${OS}/python
-pip install -r requirements.txt
+${PIP} install -r requirements.txt;
 pushd ..
-ls
-cp vsp vspaero vspscript vsploads ${PREFIX}/bin
+cp vspaero vspscript vsploads ${PREFIX}/bin
+if [[ !$(no_graphics) ]]; then
+  cp vsp ${PREFIX}/bin
+fi
 popd
 popd
