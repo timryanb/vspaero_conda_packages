b5a92ded159cbc38108a38febdd81464c6df4037
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2022-09-19 19:10:43 -0400

    Moving down to one proc compile

--- openvsp/build.sh
+++ openvsp/build.sh
@@ -13,16 +13,15 @@ cmake -DVSP_NO_GRAPHICS=${no_graphics} -DVSP_USE_SYSTEM_LIBXML2=true -DVSP_USE_S
       -DVSP_USE_SYSTEM_GLM=true -DVSP_USE_SYSTEM_GLEW=true \
       -DVSP_USE_SYSTEM_CMINPACK=true -DVSP_USE_SYSTEM_LIBIGES=false -DVSP_USE_SYSTEM_EIGEN=true \
       -DVSP_USE_SYSTEM_CODEELI=false -DVSP_USE_SYSTEM_CPPTEST=false ../Libraries -DCMAKE_BUILD_TYPE=Release
-make -j ${CPU_COUNT} VERBOSE=1
+make -j1
 cd ../build
 cmake ../src/ -DVSP_NO_GRAPHICS=${no_graphics} -DVSP_LIBRARY_PATH=../buildlibs -DCMAKE_BUILD_TYPE=Release
-make -j ${CPU_COUNT} VERBOSE=1
+make -j ${CPU_COUNT}
 make package
 # Install python interface
 pushd _CPack_Packages/${OS}/ZIP/OpenVSP-${PKG_VERSION}-${OS}/python
 ${PYTHON} -m pip install --no-deps --prefix=${PREFIX} -r requirements.txt -vv
 cd ..
-ls
 cp -v vspaero vspscript vsploads ${PREFIX}/bin
 if [[ $(no_graphics) == "false" ]]; then
   cp -v vsp ${PREFIX}/bin
