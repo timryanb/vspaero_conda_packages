ebbef0855f625e3d8b7eff757b84aad4253394e9
trbrooks <tim.r.brooks@nasa.gov>
2022-10-13 16:35:17 -0400

    Changing adept compile option to O3

--- adept/build.sh
+++ adept/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX} --disable-openmp --with-blas=openblas LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib" CXXFLAGS="${CXXFLAGS}"
+./configure --prefix=${PREFIX} CXXFLAGS="-O3 -march=native" --disable-openmp --with-blas=openblas LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
 make -j ${CPU_COUNT}
 make check
 make install
